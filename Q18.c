908469add0fb267824efaba211e05bc7870fee2b
SudhanshuMann <sudhanshu25csu373@ncuindia.edu>
2025-12-14 18:11:07 +0530

    Create Q18.c

---
+++ Q18.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, found = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n], visited[n];
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        visited[i] = 0;
+    }
+
+    for (i = 0; i < n; i++) {
+        if (visited[i])
+            continue;
+
+        int count = 1;
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                visited[j] = 1;
+                count++;
+            }
+        }
+
+        if (count > 1) {
+            printf("%d ", arr[i]);
+            found = 1;
+        }
+    }
+
+    if (!found)
+        printf("-1");
+
+    return 0;
+}
