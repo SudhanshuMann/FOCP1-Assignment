b860e0ef694dea315f4db3868834a0516ca630f0
SudhanshuMann <sudhanshu25csu373@ncuindia.edu>
2025-12-14 17:58:10 +0530

    Update Q6.c

--- Q6.c
+++ Q6.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    int rows = 5;
-    int i, j, k;
+    int choice;
+    int decimal = 0, binary, base = 1, rem;
+    long long binResult = 0;
 
-    for (i = 1; i <= rows; i++) {
-        for (j = 1; j <= i; j++) {
-            printf("%d", (j - 1) % 2);
+    printf("Choose conversion type:\n");
+    printf("1. Binary to Decimal\n");
+    printf("2. Decimal to Binary\n");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        printf("Enter a binary number: ");
+        scanf("%d", &binary);
+
+        while (binary != 0) {
+            rem = binary % 10;
+            decimal += rem * base;
+            base *= 2;
+            binary /= 10;
         }
 
-        for (k = 1; k <= (rows - i) * 2; k++) {
-            printf(" ");
+        printf("Decimal value is %d\n", decimal);
+    }
+    else if (choice == 2) {
+        printf("Enter a decimal number: ");
+        scanf("%d", &decimal);
+
+        while (decimal != 0) {
+            rem = decimal % 2;
+            binResult = binResult * 10 + rem;
+            decimal /= 2;
         }
 
-        for (j = 1; j <= i; j++) {
-            printf("%d", (j - 1) % 2);
-        }
-
-        printf("\n");
+        printf("Binary value is %lld\n", binResult);
+    }
+    else {
+        printf("Invalid choice\n");
     }
 
     return 0;
 }
+
